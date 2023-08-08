1c6eb1ab75b341de82fccfc4ac44462820002475
Darpan <darpan.kapadia@gmail.com>
2023-08-08 19:31:25 +1200

    Made bottom sheet work

--- app/src/main/java/com/dk/arcore/TryOutFurniture.kt
+++ app/src/main/java/com/dk/arcore/TryOutFurniture.kt
@@ -1,59 +1,43 @@
 package com.dk.arcore
 
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.dk.arcore.databinding.FragmentHomePageBinding
+import com.dk.arcore.databinding.FragmentTryOutFurnitureBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TryOutFurniture.newInstance] factory method to
- * create an instance of this fragment.
- */
+private const val  BOTTOM_SHEET_PEEK_HEIGHT = 50f
 class TryOutFurniture : Fragment(R.layout.fragment_try_out_furniture) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding: FragmentTryOutFurnitureBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentTryOutFurnitureBinding.bind(view)
+        setupBottomSheet()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_try_out_furniture, container, false)
-    }
+    private fun setupBottomSheet(){
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
+        bottomSheetBehavior.peekHeight = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            BOTTOM_SHEET_PEEK_HEIGHT,
+            resources.displayMetrics
+        ).toInt()
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TryOutFurniture.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TryOutFurniture().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        bottomSheetBehavior.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback(){
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                binding.bottomSheet.bringToFront()
             }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                //NO-OP
+            }
+        })
     }
 }
